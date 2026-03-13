f09d3b28115502ff09391742d00ae675dde101a1
Mario Valdez <valdezmario6@gmail.com>
2026-03-12 19:18:16 -0600

    functionality complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        //uses .lines rust builtin method to read each line
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results //return value 
 }
 
-#[cfg(test)]
+#[cfg(test)] //Markup that stablishes the beginning of the test
 mod tests {
-    use super::*;
-    #[test]
+    //Markup that defines the test
+    use super::*; //Grants access to the "Parent function" meaning the complete file
+    #[test] //Markup that defines de beginning of the test function
     fn one_result() {
         let query = "duct";
         let contents = "\
@@ -13,6 +23,13 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents)); //asserteq macro 
+        //tests the two
+        //parameters provided
+        //are strictly equal
+        //or returns an error
+        //with detailed
+        //information on why
+        //it failed.
     }
 }
